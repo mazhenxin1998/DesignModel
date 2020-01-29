17bcefd2ac512b1d5602e12f480a8232f90b415b
mazhenxin1998 <2280480546@qq.com>
2020-01-29 16:46:56 +0800

    CGLIB

--- src/main/java/com/mzx/proxy/cglib/Client.java
+++ src/main/java/com/mzx/proxy/cglib/Client.java
@@ -1,5 +1,11 @@
 package com.mzx.proxy.cglib;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
 /**
  * @author ZhenXinMa
  * @date 2020/1/29 16:35
@@ -16,6 +22,19 @@ public class Client {
         System.out.println(teach);
         System.out.println("----------");
         System.out.println(teacherDaoProxy.getClass());
+
+        System.out.println("不使用ProxyFactory----------------------");
+        TeacherDao o = (TeacherDao) Enhancer.create(teacherDao.getClass(), new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("开始代理");
+                Object invoke = method.invoke(teacherDao, objects);
+                System.out.println("代理结束");
+                System.out.println(invoke);
+                return invoke;
+            }
+        });
+        o.teach();
 
 
     }
